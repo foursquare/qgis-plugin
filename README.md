9219a7d4a40b8026cb667f6f32f0363bac8c03e5
Joonalai <joona@gispo.fi>
2021-01-19 09:54:14 +0200

    Add plugin template using qgis_plugin_tools

---
+++ README.md
@@ -0,0 +1,15 @@
+# Unfolded Studio QGIS plugin
+![](https://github.com/UnfoldedInc/UnfoldedStudio/workflows/Tests/badge.svg)
+![](https://github.com/UnfoldedInc/UnfoldedStudio/workflows/TestsLTR/badge.svg)
+[![codecov.io](https://codecov.io/github/UnfoldedInc/UnfoldedStudio/coverage.svg?branch=master)](https://codecov.io/github/UnfoldedInc/UnfoldedStudio?branch=master)
+![](https://github.com/UnfoldedInc/UnfoldedStudio/workflows/Release/badge.svg)
+
+
+### Development
+
+Refer to [development](docs/development.md) for developing this QGIS3 plugin.
+
+## License
+This plugin is licenced with
+[GNU Genereal Public License, version 2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html).
+See [LICENSE](LICENSE) for more information.
---
+++ UnfoldedStudio/test/conftest.py
@@ -0,0 +1,34 @@
+#  Gispo Ltd., hereby disclaims all copyright interest in the program Unfolded Studio QGIS plugin
+#  Copyright (C) 2021 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of Unfolded Studio QGIS plugin.
+#
+#  Unfolded Studio QGIS plugin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Unfolded Studio QGIS plugin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Unfolded Studio QGIS plugin.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html>.
+
+"""
+This class contains fixtures and common helper function to keep the test files shorter
+"""
+
+import pytest
+
+from ..qgis_plugin_tools.testing.utilities import get_qgis_app
+
+QGIS_APP, CANVAS, IFACE, PARENT = get_qgis_app()
+
+
+@pytest.fixture(scope='function')
+def new_project() -> None:
+    """Initializes new QGIS project by removing layers and relations etc."""
+    yield IFACE.newProject()
