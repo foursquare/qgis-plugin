ea7011f5d75d5dd62ab1aa67b1a61bf0b713866b
Joonalai <joona@gispo.fi>
2021-01-27 10:33:01 +0200

    Add initial UI dialog with few panels

---
+++ UnfoldedStudio/ui/export_panel.py
@@ -0,0 +1,38 @@
+#  Gispo Ltd., hereby disclaims all copyright interest in the program Unfolded Studio QGIS plugin
+#  Copyright (C) 2021 Gispo Ltd (https://www.gispo.fi/).
+#
+#
+#  This file is part of Unfolded Studio QGIS plugin.
+#
+#  Unfolded Studio QGIS plugin is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  Unfolded Studio QGIS plugin is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Unfolded Studio QGIS plugin.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html>.
+
+import logging
+
+from .base_panel import BasePanel
+from ..definitions.gui import Panels
+from ..qgis_plugin_tools.tools.resources import plugin_name
+
+LOGGER = logging.getLogger(plugin_name())
+
+
+class ExportPanel(BasePanel):
+    """
+    """
+
+    def __init__(self, dialog):
+        super().__init__(dialog)
+        self.panel = Panels.Export
+
+    def setup_panel(self):
+        pass
