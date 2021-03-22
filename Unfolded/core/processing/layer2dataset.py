ca06fd39680a66f1bde055fffcdfbf6daeafca9d
Joonalai <joona@gispo.fi>
2021-03-22 12:52:16 +0200

    #46: Use string quoting always to prevent errors in Studio import

--- Unfolded/core/processing/layer2dataset.py
+++ Unfolded/core/processing/layer2dataset.py
@@ -202,7 +202,7 @@ class LayerToDatasets(BaseConfigCreatorTask):
         options = QgsVectorFileWriter.SaveVectorOptions()
         options.driverName = "csv"
         options.fileEncoding = "utf-8"
-        options.layerOptions = ["SEPARATOR=TAB", "STRING_QUOTING=IF_NEEDED"]
+        options.layerOptions = ["SEPARATOR=TAB", "STRING_QUOTING=ALWAYS"]
         options.fieldValueConverter = converter
 
         # noinspection PyCallByClass
